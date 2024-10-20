c41de62a718f1e2500dc47e65d06a04d30984a6d
Cloud User <azureuser@vm-rhel9.lg5hv1g2xeyexpezuhup1fy1id.cx.internal.cloudapp.net>
2024-10-20 18:06:08 +0000

    vllm-rhel

--- builds/vllm/oneDNN/build/src/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/work/workspace/git/vllm/oneDNN/src
-# Build directory: /root/work/workspace/git/vllm/oneDNN/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("common")
-subdirs("cpu")
--- builds/vllm/oneDNN/build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/work/workspace/git/vllm/oneDNN/tests
-# Build directory: /root/work/workspace/git/vllm/oneDNN/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
